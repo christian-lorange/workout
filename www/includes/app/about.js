50a31dda0341b54b5732faa37e2629a600a02cc8
clorange <lorange.christian@gmail.com>
2017-12-30 09:38:38 -0700

    Fresh build of app using json pulls

---
+++ www/includes/app/about.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function ($) {
+
+    const RESTURL = 'https://104.131.144.154/CSA_demo/wp-json/'
+
+    var app = {
+        
+        init : function() {
+            
+            this.getSiteData()
+            //this.loadPosts()
+            this.loadSinglePages()
+
+            this.loadCategories()
+            this.loadEvents()
+            
+        },
+        
+        loadEvents : function() {
+            
+            $( '#main-content' ).on( 'click', '.blog-post h3', this.loadSinglePost )
+            $( '#main-content' ).on( 'click', '.blog-post .thumbnail', this.loadSinglePost )
+
+  
+            
+        },
+        
+        getSiteData : function() {
+            
+            $.get( RESTURL )
+                .done( function( response ) {
+                    $( '.site-title' ).html( response.name )
+                    $( '.description' ).html( response.description )
+                })
+                .fail( function() {
+                    alert( 'Failed to retreive data. Make sure you have an data connection' )
+                })
+
+        },
+        
+        
+          loadSinglePages : function() {
+            
+         
+            
+            var id = '48'
+            
+            //For posts the line below should be var url = RESTURL + 'wp/v2/posts/' + id + '?_embed'
+
+            var url = RESTURL + 'wp/v2/pages/' + id + '?_embed'
+            
+            $.get( url )
+                .done( function( response ) {
+
+                    
+                    var template = $( '#single-post-template' ).html()
+                    var output = $( '#main-content' )
+                                        
+                    var result = Mustache.to_html( template, response )
+                    output.html( result )
+                    
+                })
+                .fail( function() {
+                    alert( 'Cannot load this vegetable' )
+                })
+            
+        },
+        
+        loadCategories : function() {
+            
+            var url = RESTURL + 'wp/v2/categories'
+            
+            $.get( url )
+                .done( function( response ) {
+                    
+                    var categories = {
+                        categories : response
+                    }
+                    
+                    var template = $( '#blog-categories-template' ).html()
+                    var output = $( '#categories' )
+                                        
+                    var result = Mustache.to_html( template, categories )
+                    output.append( result )
+                    
+                })
+                .fail( function() {
+                    alert( 'cannot load categories' )
+                })
+            
+        },
+        
+       
+   
+        
+    }
+
+    app.init();
+
+});
---
+++ www/includes/app/master.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function ($) {
+
+    const RESTURL = 'https://104.131.144.154/CSA_demo/wp-json/'
+
+    
+});
