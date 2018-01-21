5d4417b0237bb1c692541f2f18d5aa1d5312a0b4
clorange <lorange.christian@gmail.com>
2018-01-21 14:20:32 -0700

    Revised coping to include images for posts (json wp)

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,5 +1,5 @@
 var rootURL = "https://csademo.orangehousellc.com";
-// var rootURL = "https://biplane.ru";
+//var rootURL = "https://biplane.ru";
 
 $$.ajax({
     dataType: "json",
@@ -8,9 +8,12 @@ $$.ajax({
 
         var html = "<ul>";
         $$.each( data, function( key, val ) {
+           var link = val.link;
+           var pic = val._embedded["wp:featuredmedia"][0].source_url;
+
             // Main list
             html += '<div class="card demo-card-header-pic">' +
-                    '<div style="background-image:url(' + val.id + ')" valign="bottom" class="card-header color-white no-border"></div>' +
+                    '<div style="background-image:url(' + pic + ')" valign="bottom" class="card-header color-white no-border"></div>' +
                     '<div class="card-content">' +
                         '<div class="card-content-inner">' +
                         '<p class="card-title">' + val.title.rendered + '</p>' +
@@ -18,7 +21,7 @@ $$.ajax({
                     '</div>' +
                     '</div>' +
                     '<div class="card-footer">' +
-                        '<a href="#" class="link read-blog" data-id="' + val.id + '">Подробно</a>' +
+                        '<a href="#" class="link read-blog" data-id="' + val.id + '">Read More</a>' +
                     '</div>' +
                     '</div>';
         });
@@ -43,76 +46,6 @@ var lastIndex = $$('.list-block li').length;
 var maxItems = 10;
 
 
-var counter = 2;
-// Attach 'infinite' event handler
-$$('.infinite-scroll').on('infinite', function () {
-
-    // Exit, if loading in progress
-    if (loading) return;
-
-    // Set loading flag
-    loading = true;
-
-    // Emulate 1s loading
-    setTimeout(function () {
-        // Reset loading flag
-        loading = false;
-
-        if (lastIndex >= maxItems) {
-            // Nothing more to load, detach infinite scroll events to prevent unnecessary loadings
-            myApp.detachInfiniteScroll($$('.infinite-scroll'));
-            // Remove preloader
-            $$('.infinite-scroll-preloader').remove();
-            return;
-        }
-
-        // Generate new items HTML
-        var html = '';
-
-
-        $$.ajax({
-            dataType: "json",
-            url: rootURL + "/wp-json/wp/v2/posts?page=" + counter,
-            success: function(data) {
-
-                $$.each( data, function( key, val ) {
-                    // Main list
-                    html += '<div class="card demo-card-header-pic">' +
-                        '<div style="background-image:url(' + val.ccw_thumbnail + ')" valign="bottom" class="card-header color-white no-border"></div>' +
-                        '<div class="card-content">' +
-                        '<div class="card-content-inner">' +
-                            '<p class="card-title">' + val.title.rendered + '</p>' +
-                            '<p>'+ val.excerpt.rendered +'</p>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="card-footer">' +
-                            '<a href="#" class="link read-blog" data-id="' + val.id + '">Подробно</a>' +
-                        '</div>' +
-                        '</div>';
-                });
-
-                if (data.length < 1) {
-                    myApp.detachInfiniteScroll($$('.infinite-scroll'));
-                    // Remove preloader
-                    $$('.infinite-scroll-preloader').remove();
-                }
-
-                // Append new items
-                $$('.list-block ul').append(html);
-
-            },
-            error: function(m) {
-                console.log(m)
-            }
-        });
-
-        counter++;
-
-        // Update last loaded index
-        lastIndex = $$('.list-block li').length;
-
-    }, 1000);
-});
 
 
 // On clicking button to read blog post
